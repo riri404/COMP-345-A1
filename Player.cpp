d4bbb8ef7d75e13d544d90e23b14da32e045c4f9
riri404 <76566650+riri404@users.noreply.github.com>
2022-01-28 20:33:37 +0300

    Initial cpp file

---
+++ Player.cpp
@@ -0,0 +1,40 @@
+#include "Player.h"
+//#include <Map.h>
+//#include <Cards.h>
+//#include <vector>
+
+Player::Player()
+{
+
+}
+
+Player::Player(Player& p)
+{
+}
+
+Player::~Player()
+{
+}
+
+Player& Player::operator=(const Player& p)
+{
+	// TODO: insert return statement here
+}
+
+istream& operator>>(istream& ins, Player& p1)
+{
+	// TODO: insert return statement here
+}
+
+ostream& operator<<(ostream& outs, const Player& p1)
+{
+	// TODO: insert return statement here
+}
+
+void Player::toAttack()
+{
+}
+
+void Player::toDefend()
+{
+}
