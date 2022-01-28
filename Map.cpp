41d01e1453f96bba72e7ff2791dd5f61bca298d7
jake <jake@eventdesk.co>
2022-01-28 16:45:43 -0500

    documented

--- Map.cpp
+++ Map.cpp
@@ -1,2 +1,11 @@
 #include <iostream>
 #include "Map.h"
+
+using namespace std;
+
+Territory::Territory(int territoryNumber, string territoryName, string continentName, int armyValue): 
+  territoryNumber(territoryNumber), 
+  territoryName(territoryName), 
+  continentName(continentName), 
+  armyValue(armyValue) { cout << "Territory " << territoryName << " created" << endl; }
+
--- Map.h
+++ Map.h
@@ -5,12 +5,20 @@
 #include <vector>
 using namespace std;
 
+// x and y coordinate of the circle that will be put on a territory
+struct Point {
+  int x;
+  int y;
+}
+
 // territory class which contains info about the territory, and the player who owns it
 class Territory {
   int territoryNumber;
+  int continentNumber;
   string continentName;
   string territoryName;
   int armyValue;
+  Point coords;
   // Player* p
 public:
   Territory(int, string, string, int);
@@ -22,18 +30,22 @@ public:
 class Map {
   map<int, vector<int>> adjList; // key represents the territory and value represent the adjacent territories
   map<int, Territory*> territories; // maps territory number to the territory object
+  map<int, vector<int>> continents; // maps continent number to a vector of territory numbers
   int numTerritories;
 public:
   Map();
   ~Map();
   Map(const Map&);
   Map& operator=(const Map&);
-  bool validate() const;
+  bool validate() const; // TODO: validate by going through the graph, counting each nodes that has not been visited before and comparing with total number of nodes, if equal, graph is connected, otherwise no
 };
 
 // copy constructor and copy assignment operator omitted because it makes no sense
 // only purpose is to take a .map file and return a map object
+// for continents, create an array, and array[index] = continent name, continent number = index + 1
 class MapLoader {
+  pair<int, vector<int>> readBorder(string);
+
 public:
   MapLoader();
   Map* loadMap(const string&);
