b62dd4573093f7f285411b7faf5bd430567588ae
jake <jake@eventdesk.co>
2022-01-28 16:01:14 -0500

    partially complete .h file

--- Map.h
+++ Map.h
@@ -1,41 +1,42 @@
 #ifndef MAP_H
 #define MAP_H
 
+#include <map>
 #include <vector>
 using namespace std;
 
-class Player;
-
+// territory class which contains info about the territory, and the player who owns it
 class Territory {
-  int nodeIndex;
+  int territoryNumber;
   string continentName;
-  string territoryName; // i think territory is country
+  string territoryName;
   int armyValue;
-  Player* p; // owned by player p
-  vector<Territory*> adjacentTerritories;
+  // Player* p
 public:
-  Territory();
+  Territory(int, string, string, int);
   Territory(const Territory&);
   Territory& operator=(const Territory&);
 };
 
+// map is implemented as sort of a graph, using adjacency list, where the territory numbers are used as the value in the adjacency list
 class Map {
-  vector<vector<int>>adjacencyMatrix;
-  vector<Territory*> territories;
-  vector<vector<Territory*>> continents;
+  map<int, vector<int>> adjList; // key represents the territory and value represent the adjacent territories
+  map<int, Territory*> territories; // maps territory number to the territory object
+  int numTerritories;
 public:
   Map();
   ~Map();
   Map(const Map&);
   Map& operator=(const Map&);
+  bool validate() const;
 };
 
+// copy constructor and copy assignment operator omitted because it makes no sense
+// only purpose is to take a .map file and return a map object
 class MapLoader {
-  MapLoader();
 public:
-  static Map* loadMap(const string&);
+  MapLoader();
+  Map* loadMap(const string&);
 };
-
-
 
 #endif
