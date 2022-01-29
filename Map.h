bb4172504c6602d43f026d5d2ec55f02953240a2
jake <jake@eventdesk.co>
2022-01-29 18:31:04 -0500

    removed std::map since its not allowed

--- Map.h
+++ Map.h
@@ -5,6 +5,15 @@
 #include <vector>
 using namespace std;
 
+class Player;
+
+// simple struct to hold 2 values (key and value)
+template <class K, class V>
+struct Pair {
+  K key;
+  V value;
+}
+
 // x and y coordinate of the circle that will be put on a territory
 struct Point {
   int x;
@@ -19,18 +28,17 @@ class Territory {
   string territoryName;
   int armyValue;
   Point coords;
-  // Player* p
+  Player* player
 public:
   Territory(int, string, string, int);
   Territory(const Territory&);
   Territory& operator=(const Territory&);
 };
 
-// map is implemented as sort of a graph, using adjacency list, where the territory numbers are used as the value in the adjacency list
+// map is implemented as a graph, using adjacency list
 class Map {
-  map<int, vector<int>> adjList; // key represents the territory and value represent the adjacent territories
-  map<int, Territory*> territories; // maps territory number to the territory object
-  map<int, vector<int>> continents; // maps continent number to a vector of territory numbers
+  vector<Pair<int, vector<Territory*>>> territories;
+  vector<Pair<int, vector<Territory*>>> continents;
   int numTerritories;
 public:
   Map();
