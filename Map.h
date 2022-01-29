15ed96271c259e3f2d81327c1697a000e53eb0ba
jake <jake@eventdesk.co>
2022-01-29 18:53:41 -0500

    added continent class

--- Map.h
+++ Map.h
@@ -1,18 +1,11 @@
 #ifndef MAP_H
 #define MAP_H
 
-#include <map>
+#include <utility>
 #include <vector>
 using namespace std;
 
 class Player;
-
-// simple struct to hold 2 values (key and value)
-template <class K, class V>
-struct Pair {
-  K key;
-  V value;
-}
 
 // x and y coordinate of the circle that will be put on a territory
 struct Point {
@@ -35,10 +28,16 @@ public:
   Territory& operator=(const Territory&);
 };
 
+class Continent {
+  int continentNumber;
+  int armyValue;
+  vector<Territory*> territories;
+}
+
 // map is implemented as a graph, using adjacency list
 class Map {
-  vector<Pair<int, vector<Territory*>>> territories;
-  vector<Pair<int, vector<Territory*>>> continents;
+  vector<pair<int, vector<Territory*>>> adjList; // adjacency list, key is the territory number and the value is a vector of adjacent territories
+  vector<pair<int, Continent*>> territories; // territories as a set of countries, each country mapped with its country number
   int numTerritories;
 public:
   Map();
@@ -48,12 +47,8 @@ public:
   bool validate() const; // TODO: validate by going through the graph, counting each nodes that has not been visited before and comparing with total number of nodes, if equal, graph is connected, otherwise no
 };
 
-// copy constructor and copy assignment operator omitted because it makes no sense
-// only purpose is to take a .map file and return a map object
-// for continents, create an array, and array[index] = continent name, continent number = index + 1
 class MapLoader {
-  pair<int, vector<int>> readBorder(string);
-
+  pair<int, vector<Territory*>> readBorder(string);
 public:
   MapLoader();
   Map* loadMap(const string&);
