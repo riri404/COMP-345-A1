3e5c81b1c8fdcaf6f8de5c4526d5c6ca3600b37b
jake <jake@eventdesk.co>
2022-01-29 19:08:37 -0500

    added default values to members

--- Map.h
+++ Map.h
@@ -9,36 +9,37 @@ class Player;
 
 // x and y coordinate of the circle that will be put on a territory
 struct Point {
-  int x;
-  int y;
+  int x = 0;
+  int y = 0;
 }
 
 // territory class which contains info about the territory, and the player who owns it
 class Territory {
-  int territoryNumber;
-  int continentNumber;
-  string continentName;
-  string territoryName;
-  Point coords;
-  Player* player
+  int territoryNumber = 0;
+  int continentNumber = 0;
+  string continentName = "";
+  string territoryName = "";
+  Point coords = {0, 0};
+  Player* player = nullptr;
 public:
+  Territory();
   Territory(int, string, string, int);
   Territory(const Territory&);
   Territory& operator=(const Territory&);
 };
 
 class Continent {
-  int continentNumber;
-  int armyValue;
-  vector<Territory*> territories;
+  int continentNumber = 0;
+  int armyValue = 0;
+  vector<Territory*> territories = {};
 }
 
 // map is implemented as a graph, using adjacency list
 class Map {
-  vector<pair<int, vector<int>>> adjList; // adjacency list, key is the territory number and the value is a vector of adjacent territory numbers
-  vector<Territory*> territories;
-  vector<Continent*> continents;
-  int numTerritories;
+  vector<pair<int, vector<Territory*>>> adjList = {}; // adjacency list, key is the territory number and the value is a vector of adjacent territory numbers
+  vector<Territory*> territories = {};
+  vector<Continent*> continents = {};
+  int numTerritories = 0;
 public:
   Map();
   ~Map();
