5034c8da836c7c1e5926dc2186807a6ae10ef74f
pamonica <73133587+pamonica@users.noreply.github.com>
2022-02-01 11:31:13 -0500

    Update Orders.h

--- Orders.h
+++ Orders.h
@@ -8,17 +8,17 @@ using namespace std;
 //If the order has been executed, it should also output the effect of the order, stored as a string. 
 class Order{
     public:
+        string name;
+        int id;
+
         //Constructor and destructor
         Order();
         ~Order();
 
-        //Member functions
-        bool validate();
-        void execute();
-
         //Copy constructor, assignement and stream insertion operator
-        Order(Order& anOrder);
+        Order(Order& anotherOrder);
         Order& operator =(const Order &anOrder);
+        friend istream& operator>>(istream&, Order&);
         friend ostream& operator<<(ostream&, const Order&);  //Lecture 3, p.7
     private:
         //...
@@ -34,10 +34,15 @@ class Deploy : public Order{
         Deploy();
         ~Deploy();
 
+        //Copy constructor, assignement and stream insertion operator
+        Deploy(Deploy& aDeploy);
+        Deploy& operator =(const Deploy &aDeploy);
+        friend istream& operator>>(istream&, const Deploy&);
+        friend ostream& operator<<(ostream&, const Deploy&);  //Lecture 3, p.7
+
         //Member functions
         bool validate();
         void execute();
-
 };
 
 class Advance : public Order{
@@ -46,10 +51,15 @@ class Advance : public Order{
         Advance();
         ~Advance();
 
+        //Copy constructor, assignement and stream insertion operator
+        Advance(Advance& anAdvance);
+        Advance& operator =(const Advance &anAdvance);
+        friend istream& operator>>(istream&, const Advance&);
+        friend ostream& operator<<(ostream&, const Advance&);  //Lecture 3, p.7
+
         //Member functions
         bool validate();
         void execute();
-
 };
 
 class Bomb : public Order{
@@ -57,6 +67,12 @@ class Bomb : public Order{
         //Constructor and destructor
         Bomb();
         ~Bomb();
+
+        //Copy constructor, assignement and stream insertion operator
+        Bomb(Bomb& aBomb);
+        Bomb& operator =(const Bomb &aBomb);
+        friend istream& operator>>(istream&, const Bomb&);
+        friend ostream& operator<<(ostream&, const Bomb&);  //Lecture 3, p.7
 
         //Member functions
         bool validate();
@@ -69,9 +85,15 @@ class Blockade : public Order{
         Blockade();
         ~Blockade();
 
+        //Copy constructor, assignement and stream insertion operator
+        Blockade(Blockade& aBlockade);
+        Blockade& operator =(const Blockade &aBlockade);
+        friend istream& operator>>(istream&, const Blockade&);
+        friend ostream& operator<<(ostream&, const Blockade&);  //Lecture 3, p.7
+
         //Member functions
         bool validate();
-        void execute();
+        void execute();    
 };
 
 class Airlift : public Order{
@@ -79,6 +101,12 @@ class Airlift : public Order{
         //Constructor and destructor
         Airlift();
         ~Airlift();
+
+        //Copy constructor, assignement and stream insertion operator
+        Airlift(Airlift& anAirlift);
+        Airlift& operator =(const Airlift &anAirlift);
+        friend istream& operator>>(istream&, const Airlift&);
+        friend ostream& operator<<(ostream&, const Airlift&);  //Lecture 3, p.7
 
         //Member functions
         bool validate();
@@ -91,9 +119,15 @@ class Negotiate : public Order{
         Negotiate();
         ~Negotiate();
 
+        //Copy constructor, assignement and stream insertion operator
+        Negotiate(Negotiate& aNegotiate);
+        Negotiate& operator =(const Negotiate &aNegotiate);
+        friend istream& operator>>(istream&, const Negotiate&);
+        friend ostream& operator<<(ostream&, const Negotiate&);  //Lecture 3, p.7
+
         //Member functions
         bool validate();
-        void execute();
+        void execute();        
 };
 
 
@@ -104,18 +138,21 @@ class OrdersList{
     public:
         //Constructor and destructor
         OrdersList();
+        OrdersList(vector<Order>);
         ~OrdersList();
-
-        //Member functions
-        void remove();
-        void move();
-    
-    private:
-        //The OrdersList class contains a list of Order objects
-        vector<Order> listOfOrders;
 
         //Copy constructor, assignement and stream insertion operator
         OrdersList(OrdersList& anOrdersList);
         OrdersList& operator =(const OrdersList &anOrdersList);
+        friend istream& operator>>(istream&, const OrdersList&);
         friend ostream& operator<<(ostream&, const OrdersList&);  //Lecture 3, p.7
+
+        //Member functions
+        void addToListOfOrders(Order);
+        void move(int, int);
+        void remove(int);
+
+    private:
+        //The OrdersList class contains a list of Order objects
+        vector<Order> listOfOrders;
 };
