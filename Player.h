fb1bf8c624b5352432cf13251a682f9e35d1267a
riri404 <76566650+riri404@users.noreply.github.com>
2022-02-05 01:12:19 +0300

    Most recent Player.h 02/05

--- Player.h
+++ Player.h
@@ -1,16 +1,17 @@
 #pragma once
-//#include <Orders.h>
-//#include <Map.h>
-//#include <Cards.h>
-//#include <vector>
+#include <Orders.h>
+#include <Map.h>
+#include <Cards.h>
+#include <vector>
 #include <iostream>
 using namespace std;
-
+class Order;
 
 class Player
 {
 public:
 	// contructors
+	Player(); // default constructor
 	Player(); // default constructor
 	Player(Player& p); //copy constructor
 	~Player(); //destructor
@@ -20,14 +21,23 @@ public:
 	friend istream& operator >>(istream& ins, Player& p);
 	friend ostream& operator <<(ostream& outs, const Player& p);
 
-	void toAttack(); // method to attck; returns orders list
-	void toDefend(); // method to defend; returns ordrs list
+	vector<Territory*> toAttack(); // method to attck; returns orders list
+	vector<Territory*> toDefend(); // method to defend; returns ordrs list
 	//void issueOrder(Orders order);
 
 private:
 	//attributes/ variables
-	Player playerID;
-	//vector<Territory> territoryList; 
-	//OrdersList orderList; OR vector<OrdersList> orderList
-	//Hand hand; OR vector<Hand> hand;
+	string* name;
+	Player* playerID;
+	vector<Territory*> territoryList; 
+	OrdersList* orderList;
+	Hand* cards; 
+
+	//friend classes
+	friend class Card;
+	friend class GameEngine;
+	friend class Map;
+
 };
+
+
