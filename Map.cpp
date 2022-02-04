302ef1ccbd24940fade172e48ef29ad5a195e8dc
jake <jake@eventdesk.co>
2022-02-04 16:05:31 -0500

    completed territory and continent

--- Map.cpp
+++ Map.cpp
@@ -1,41 +1,48 @@
 #include <iostream>
+#include <string>
 #include "Map.h"
 
 using namespace std;
-
+//---------------------Territory-----------------------
+// **Destructor**
 Territory::~Territory() {
-  for (Territory* t : adjacentTerritories) delete t;
+  // no delete needed
 }
 
-Territory::Territory(int armies, int id, string name) {
+Territory::Territory() {
+  playerId = -1;
+  armies = 0;
+  id = -1;
+  name = "";
+}
+
+// **Constructor**
+Territory::Territory(int id, string name) {
   this->playerId = -1;
-  this->armies = armies;
+  this->armies = 0; // idk whats the starting value
   this->id = id;
   this->name = name;
 }
-
+//  **Copy Constructor** 
+//( other is the obj that we want to copy from).
 Territory::Territory(const Territory& other) {
   playerId = other.playerId;
   armies = other.armies;
   id = other.id;
   name = other.name;
-  for (Territory* t : other.adjacentTerritories) {
-    adjacentTerritories.push_back(new Territory(*t));
-  }
-}
-
+}// because we have pointers to atributrs for this class ==>
+// https://www.youtube.com/watch?v=PXcRe-W2w7s
+// the function operator= is a member of the class Territory we have
+//  to add the class name and scoop operator like that (Territory::)
+// **overload Assignment (operator = ) for Territory**
 Territory& Territory::operator=(const Territory& rhs) {
   playerId = rhs.playerId;
   armies = rhs.armies;
   id = rhs.id;
   name = rhs.name;
-  for (Territory* t : adjacentTerritories) delete t;
-  adjacentTerritories.clear();
-  for (Territory* t : rhs.adjacentTerritories) {
-    adjacentTerritories.push_back(new Territory(*t));
-  }
+  return *this;
 }
-
+// **Assignment operator << to print Territory object.**
 ostream& operator<<(ostream& out, const Territory& territory) {
   out << "Name: " << territory.name << " (" << territory.id << ")" << endl;
   out << "Armies: " << territory.armies << endl;
@@ -43,36 +50,48 @@ ostream& operator<<(ostream& out, const Territory& territory) {
   return out;
 }
 
+//----------------------Continent--------------------------
+// **destructor**
 Continent::~Continent() {
   for (Territory* t : territories) delete t;
 }
 
+Continent::Continent() {
+  this->armyValue = -1;
+  this->id = -1;
+  this->name = "";
+}
+
+// **Constructor**
 Continent::Continent(int armyValue, int id, string name) {
   this->armyValue = armyValue;
   this->id = id;
   this->name = name;
 }
 
+//  **Copy Constructor**
 Continent::Continent(const Continent& other) {
   armyValue = other.armyValue;
   id = other.id;
   name = other.name;
+
   for (Territory* t : other.territories) {
     territories.push_back(new Territory(*t));
   }
-}
-
+}// because we have pointers to atributrs for this class ==>
+// **overload Assignment (operator=) for Continent**
 Continent& Continent::operator=(const Continent& rhs) {
-  armyValue = rhs.armyValue;
-  id = rhs.id;
-  name = rhs.name;
-  for (Territory* t : territories) delete t;
-  territories.clear();
-  for (Territory* t : rhs.territories) {
-    territories.push_back(new Territory(*t));
-  }
-}
+    armyValue = rhs.armyValue;
+    id = rhs.id;
+    name = rhs.name;
 
+    for (Territory* t : territories) delete t;
+    for (Territory* t : rhs.territories) {
+        territories.push_back(new Territory(*t));
+    }
+    return *this;
+}
+//**Assignment operator << to print Continent object.**
 ostream& operator<<(ostream& out, const Continent& continent) {
   out << "Name: " << continent.name << " (" << continent.id << ")" << endl;
   out << "Army value: " << continent.armyValue << endl;
@@ -81,4 +100,25 @@ ostream& operator<<(ostream& out, const Continent& continent) {
     out << *t << endl;
   }
   return out;
+}
+
+//-----------------------------Map---------------------------
+Map::~Map() {
+  for (Continent* c : continents) delete c;
+  for (Territory* t : territories) delete t;
+  for (auto list : adjList) {
+    for (Territory* t : list) delete t;
+  }
+}
+
+Map::Map() {
+
+}
+//---------------------------Map loader----------------------
+
+void Map::readMap(const string& file) {
+  // you can work here
+  // open file, google std::getline, std::istream for this part
+  // https://stackoverflow.com/questions/29097127/c-reading-file-line-by-line
+  // for help
 }
--- Map.h
+++ Map.h
@@ -5,54 +5,95 @@
 using namespace std;
 
 class Player;
-
+//---------------------Territory-----------------------
 class Territory {
-  int id = 0;
-  string name = "";
-  int playerId = -1;  // this territory is owned by player with this id
-  int armies = 0;
-  vector<Territory*> adjacentTerritories = {};
+  string name;             // territorys name.
+  int id;                   // territorys ID.
+  int playerId;             // territory owned by player with this id
+  int armies;               // # of armies
+  /* constructors */
 public:
-  ~Territory();
-  Territory(int, int, sring);
-  Territory(const Territory&);
-  Territory& operator=(const Territory&);
-  friend ostream& operator<<(ostream&, const Territory&);
+  ~Territory();					        // Destructor
+	Territory(); 									// Default Constructor
+  Territory(int, string);  // Constructor
+  Territory(const Territory&);  // Copy Constructor
+  Territory& operator=(const Territory&);//!! // Assignment operator Overloading 
+	                 // when data members are type pointer and we need deep copies
+	                 // and we have a copy constructor.
+	                 // https://www.youtube.com/watch?v=ieD3l--qgK4
+  friend ostream& operator<<(ostream&, const Territory&);//!!// we use this when
+                     //we want to print a user defined class object using COUT.
+					 //https://www.youtube.com/watch?v=2972LRdyquk
 };
 
+//----------------------Continent--------------------------
 class Continent {
-  int id = 0;
-  string name = "";
-  int armyValue = 0;
-  vector<Territory*> territories = {};
+	int id;
+	int armyValue;
+	string name;
+	vector<Territory*> territories;
+	/* constructors */
 public:
-  ~Continent();
-  Continent(int, int, string);
-  Continent(const Continent&);
-  Continent& operator=(const Continent&);
-  friend ostream& operator<<(ostream&, const Continent&);
-}
+	~Continent();                      // Destructor
+	Continent(); // added              // Default Constructor
+	Continent(int, int, string);       // Constructor
+	Continent(const Continent&);       // Copy Constructor
+	Continent& operator=(const Continent&); // Assignment operator Overloading 
+	                 // when data members are type pointer and we need deep copies
+	                 // and we have a copy constructor.
+	                 // https://www.youtube.com/watch?v=ieD3l--qgK4
+	friend ostream& operator<<(ostream&, const Continent&);//!! // we use this when
+                     //we want to print a user defined class object using COUT.
+					 //https://www.youtube.com/watch?v=2972LRdyquk
+};
 
 // an adjacency list is used to represent the graph, 
-// implemented with a vector of territories, where each territory contains a vector of adjacent territories
+//-----------------------------Map---------------------------
 class Map {
-  vector<Continent*> continents = {}; // sub graphs
-  vector<Territory*> territories = {}; // adjacency list
-  int numTerritories = 0;
+	vector<Continent*> continents; // sub graphs
+	// continents[continentId - 1] to access continent of that id
+	vector<vector<Territory*>> adjList; // adjacency list
+	// we are going to use the id of the territory as index
+	// territories[0] === adj territories of territory with id 1
+	// so, territories[territoryId - 1] used to access ajd territories
+	vector<Territory*> territories;
+	// all territories stored, for now to have a map from territoryId to its name
+	int numTerritories;
 public:
-  Map();
-  ~Map();
-  Map(const Map&);
-  Map& operator=(const Map&);
-  void addTerritory(Territory* territory);
-  void addBorder(Territory* t1, Territory* t2);
-  bool validate() const; // TODO: validate by going through the graph, counting each nodes that has not been visited before and comparing with total number of nodes, if equal, graph is connected, otherwise no, do this for territories and continents, and finally check that continents contains no duplicate territories
+	~Map();                      // Destructor
+	Map();                       // Default Constructor
+	Map(const Map&);             // Copy Constructor 
+	Map& operator=(const Map&);  // Assignment operator Overloading
+	friend ostream& operator << (ostream& out, const Map& );//added//
+	void addContinentToMap(Continent* continent);  //added//
+	void addTerritory(Territory* territory);
+	void addBorder(Territory* t1, Territory* t2);
+	
+	bool validate() const; // TODO: validate by going through the graph, 
+	                       //counting each nodes that has not been visited 
+	                       //before and comparing with total number of nodes
+	                       //, if equal, graph is connected, otherwise no, 
+	                       //do this for territories and continents, 
+		                  	 //and finally check that continents contains 
+	                       //no duplicate territories.
 };
-
+//---------------------------Map loader--------------------------
 class MapLoader {
+	string fileName; // to read the file name???
+	vector<string> borders; // store each line of the border section in the vector
+	// ex. ["1 2 3 4", "2 12 10 5", ...]
+	vector<string> continents; // same as borders
+	vector<string> countries;  // same as borders
+	// check source_maps folder for an example file
+	// we will obtain all the data first, then we will process the data and create the objects
 public:
+  ~MapLoader();//do we need this?? 
   MapLoader();
-  Map* loadMap(const string&);
+  MapLoader(const MapLoader&);// do we need this??
+  MapLoader& operator = (const MapLoader& );// do we need this??
+  friend ostream& operator<<(ostream& out, const MapLoader&); //added//ostream operator
+  void readMap(const string&); // TODO: open any .map file and fill up the 3 vectors (borders, continents and countries)
+	void processData(); // not completed yet
 };
 
 #endif
