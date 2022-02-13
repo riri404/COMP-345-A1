4337e63bed87725f30dba9934d22d2673b935c45
riri404 <76566650+riri404@users.noreply.github.com>
2022-02-13 13:48:26 -0500

    most recent 02/13

--- Player.h
+++ Player.h
@@ -5,30 +5,37 @@
 #include <vector>
 #include <iostream>
 using namespace std;
-class Order;
+
+class Orders;
+class OrderList;
+class Card;
+class Hand;
 
 class Player
 {
 public:
 	// contructors
 	Player(); // default constructor
-	Player(); // default constructor
-	Player(Player& p); //copy constructor
+	Player(int id,string name, vector<Territory*> territoryList, Hand* cards, OrdersList* orderlist); //Constructor
+	Player(const Player& p); //copy constructor
 	~Player(); //destructor
 
-	//methods
+	// operators
 	Player& operator =(const Player& p);
 	friend istream& operator >>(istream& ins, Player& p);
 	friend ostream& operator <<(ostream& outs, const Player& p);
 
+	//methods
 	vector<Territory*> toAttack(); // method to attck; returns orders list
 	vector<Territory*> toDefend(); // method to defend; returns ordrs list
-	//void issueOrder(Orders order);
+	void issueOrder(string order);
+	void setOrder(Orders*);
+
 
 private:
 	//attributes/ variables
 	string* name;
-	Player* playerID;
+	int* playerID;
 	vector<Territory*> territoryList; 
 	OrdersList* orderList;
 	Hand* cards; 
@@ -39,5 +46,6 @@ private:
 	friend class Map;
 
 };
+
 
 
