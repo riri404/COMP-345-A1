47daab94553e7860cb8df9512838d36c23caf7ad
riri404 <76566650+riri404@users.noreply.github.com>
2022-02-13 20:20:41 -0500

    Update PlayerDriver.cpp

--- PlayerDriver.cpp
+++ PlayerDriver.cpp
@@ -1,74 +1,75 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include "Player.h"
-#include <Territory.h>
 #include "Orders.h"
 #include "Cards.h"
+using namespace std;
 
-int main {
 
- //creating arbitary list of territories
-  Territory* t1 = new Territory(1,"America");
-  Territory* t2 = new Territory(2,"Canada");
-  vector<Territory*> territoryList = {t1, t2};
+int main(){
 
-  //creating arbitary hand
-  Hand* cards;
-  string c1 = "Deploy";
-  string c2 = "Advance";
-  cards.push_back(&c1);
-  cards.push_back(&c2);
+    int playerID = 12345;
+    int* id = &playerID;
 
-  // creating arbitary list of orders
-  OrderList orderList;
-  Order o1, o2;
-  Order* ptr1 = &o1;
-  Order* ptr2 = &o2;
-  orderList.addToListOfOrders(ptr1);
-  orderList.addToListOfOrders(ptr2);
+    string name = "badgirlriri";
+    string* n = &name;
 
-  OrderList* orderList2 = new OrderList;
-  orderList2 = &orderList;
+    //creating arbitary list of territories
+     Territory * t1 = new Territory();
+     Territory* t2 = new Territory();
+     vector<Territory*> territoryList = {t1, t2};
 
-  //creating arbitary card and then hand
-  Bomb bomb;
-  Card* ptrBomb = &bomb;
-  vector<Card*> myHand;
 
-  myHand.push_back(ptrBomb);
+     // creating arbitary list of orders
+     OrdersList orderList;
+     //Order o1, o2;
+     //Order* ptr1 = &o1;
+     //Order* ptr2 = &o2;
+     //orderList.addToListOfOrders(ptr1);
+    // orderList.addToListOfOrders(ptr2);
 
-  //  MyCollectionOfCards2.push_back(PointerBlockade2);
-  Hand cards(myHand);
-  Hand* cardsptr = &cards;
+     OrdersList* orderList2 = new OrdersList;
+     orderList2 = &orderList;
 
-  cout << "Creating player\n";
-  Player player1 = new Player(12345,"mark", territoryList, cardsptr, orderList2);
-  cout << player1 << endl;
+     //creating arbitary card and then hand
+     Hand x;
+     //Cards* y= new Cards();{};
+     //Cards* z= new Cards();
+     //x.setHand(y);
+     //x.setHand(z);
+     Hand hand = x ;
+     Hand* h = &hand;
 
-  //testing toAttack()
-  cout << "testing toAttack() method---------------------------------\n";
-  for (Territory* t : player1.toAttack()) {
-      cout << (*t) << endl;
-  }
-  
-  
-//testing toDefend()
-  cout << "testing toDefend() method--------------------------------\n";
-  for (Territory* t : player1.toDefend()) {
+     
 
-      cout << (*t) << endl;
-  }
+     std:: cout << "Creating player\n";
+     Player* player1 = new Player( id, n, territoryList, h, orderList2);
+     std::cout << player1 << endl;
 
- 
+     //testing toAttack()
+     std::cout << "testing toAttack() method---------------------------------\n";
+     for (Territory* t : player1->toAttack()) {
+         std::cout << (*t) << endl;
+     }
 
-  cout << "testing issueOrder() method---------------------------------\n";
-  player1.issueOrder("Bomb");
 
-  //delete player that created
-  delete player;
+     //testing toDefend()
+     std::cout << "testing toDefend() method--------------------------------\n";
+       for (Territory* t : player1->toDefend()) {
 
-  player = nullptr;
+           std::cout << (*t) << endl;
+       }
+
+
+
+       std::cout << "testing issueOrder() method---------------------------------\n";
+       player1->issueOrder("Bomb");
+
+       //delete player that created
+       delete player1;
+
+       player1 = nullptr;
 
 }
-
 
